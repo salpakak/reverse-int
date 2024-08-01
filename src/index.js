59f987131086345bdbcade530c5b57e8cec473fb
salpakak <salpakak@gmail.com>
2024-08-02 02:37:31 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let str = n.toString();
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
 }
